9f798d2d7310d11f25d712de3d7ccd60ca3cd16d
Vincent <8756@ait.nsw.edu.au>
2023-03-08 18:11:26 +1100

    Valid Form

--- Screens/SignUp.js
+++ Screens/SignUp.js
@@ -1,55 +1,108 @@
-import { Button, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
-import { TextInput } from 'react-native-web';
+import { View, Text, TextInput, TouchableOpacity, StyleSheet } from "react-native";
+import { useEffect, useState } from 'react'
 
-export function SignUpScreen( props ) {
-    return (
-      <View>
-        <Text style={style.title}>Sign Up Screen for an account</Text>
-        <View style ={style.inputGroup}>
-          <Text>Email address</Text>
-          <TextInput
-              style={style.input}
-              placeholder ="email@domain.com"
-          />
-        </View>
-        <View style ={style.inputGroup}>
-          <Text >Password</Text>
-          <TextInput 
-              style={style.input}
-              placeholder = "minimum 8 characters"
-          />
-        </View>
-        <TouchableOpacity style={style.button}>
-          <text style={style.buttontext}>Sign Up</text>
-        </TouchableOpacity>
-      </View>
-    );
-  }
+export function SignUpScreen ( props ) {
+    const [ email, setEmail ] = useState("")
+    const [ validEmail, setValidEmail ] = useState(false)
+    const [ password, setPassword ] = useState("")
+    const [ validPassword, setValidPassword ] = useState(false)
+    const [ validForm, setValidForm ] = useState( false)
 
-  const style = StyleSheet.create({
-    page:{
-      marginHorizontal: 60,
+    useEffect( () => {
+        if( email.indexOf('@') > 0 ) {
+            setValidEmail( true )
+        }
+        else {
+            setValidEmail( false )
+        }
+    }, [email] )
+
+    useEffect( () => {
+       if( password.length >= 8 ) {
+            setValidPassword( true )
+       }
+       else {
+            setValidPassword( false )
+       }
+    }, [password])
+
+    useEffect( () => {
+        if( validEmail && validPassword ) {
+            setValidForm(true)
+        }
+        else {
+            setValidForm(false)
+        }
+    })
+
+    return(
+        <View style={ styles.page }>
+            <Text style={ styles.title }>Sign up for an account</Text>
+            <View style={ styles.inputGroup }>
+                <Text>Email address</Text>
+                <TextInput 
+                    style={ (validEmail) ? styles.validInput : styles.input} 
+                    placeholder="you@domain.com"
+                    value={ email }
+                    onChangeText={ (emailText) => setEmail(emailText) }
+                />
+            </View>
+            <View style={ styles.inputGroup }>
+                <Text>Password</Text>
+                <TextInput 
+                    style={ (validPassword) ? styles.validInput : styles.input}  
+                    placeholder="minimum 8 characters"
+                    value={ password }
+                    onChangeText={ (pwText) => setPassword(pwText) }
+                    secureTextEntry={true}
+                />
+            </View>
+            <TouchableOpacity 
+                style={ (validForm) ? styles.button : styles.buttonDisabled }  
+                disabled={ (validForm) ? false : true}
+            >
+                <Text style={ styles.buttonText }>Sign up</Text>
+            </TouchableOpacity>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    page: {
+        marginHorizontal: 60,
     },
     title: {
-      fontSize: 20,
-      textAlign: 'center',
-      margin:20,
+        fontSize: 20,
+        textAlign: 'center',
+        marginTop: 20,
     },
-    input:{
-      backgroundColor: "#ffffff",
-      padding: 5,
+    input: {
+        backgroundColor: "#ffffff",
+        padding: 5,
+        borderWidth: 1,
+        borderColor: "#cccccc",
     },
-    inputGroup:{
-      marginVertical:5,
-
+    validInput: {
+        borderColor: "green",
+        borderWidth: 1,
+        backgroundColor: "#ffffff",
+        padding: 5,
     },
-    button:{
-      backgroundColor: "#000000",
-      padding: 10,
-      marginVertical: 10,
+    inputGroup: {
+        marginVertical: 5,
     },
-    buttontext:{
-      color: "#ff212f"
+    button: {
+        backgroundColor: "#000000",
+        padding: 10,
+        marginVertical: 10,
+    },
+    buttonText: {
+        color: "#ffffff",
+        textAlign: "center"
+    },
+    buttonDisabled: {
+        backgroundColor: "#666666",
+        padding: 10,
+        marginVertical: 10,
     }
-
-  })
+})
