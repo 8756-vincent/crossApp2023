2e086dd49f1a08c6a7d92f2f00e555b47181d4da
Vincent <8756@ait.nsw.edu.au>
2023-03-15 16:58:37 +1100

    FireBase

--- App.js
+++ App.js
@@ -2,20 +2,52 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-
-//screens
-import { HomeScreen } from './Screens/HomeScreen';
-import { SignUpScreen } from './Screens/SignUp';
+import { useState, useEffect } from 'react';
+// screens
+import { HomeScreen } from './screens/HomeScreen';
+import { SignUpScreen } from './screens/SignUp';
+import { SignInScreen } from './screens/SignIn';
+// firebase
+import { firebaseConfig } from './config/Config';
+import { initializeApp } from 'firebase/app'
+import { 
+  getAuth, 
+  createUserWithEmailAndPassword,
+  onAuthStateChanged
+} from "firebase/auth"
 
 const Stack = createNativeStackNavigator();
 
+const FBapp = initializeApp( firebaseConfig )
+const FBauth = getAuth( FBapp )
+
 export default function App() {
+  const [auth,setAuth] = useState()
+
+  onAuthStateChanged( FBauth, (user) => {
+    if( user ) {
+      setAuth( user )
+    }
+    else {
+      setAuth( null )
+    }
+  })
+
+  const SignUp = ( email, password ) => {
+    createUserWithEmailAndPassword( FBauth, email, password )
+    .then( (userCredential) => console.log(userCredential) )
+    .catch( (error) => console.log(error) )
+  }
+
   return (
     <NavigationContainer>
-      <Stack.Navigator>
+     <Stack.Navigator>
+        <Stack.Screen name="Signup">
+          { (props) => <SignUpScreen {...props} handler={SignUp} authStatus={auth} /> }
+        </Stack.Screen>
+        <Stack.Screen name="Signin" component={SignInScreen} />
         <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="SignUp" component={SignUpScreen} />
-      </Stack.Navigator>        
+      </Stack.Navigator>
     </NavigationContainer>
   );
 }
