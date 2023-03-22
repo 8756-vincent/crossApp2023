e4b5b04f88024536db3806d81286a5a911ffe645
Vincent <8756@ait.nsw.edu.au>
2023-03-22 18:15:15 +1100

    Added UID

--- App.js
+++ App.js
@@ -2,20 +2,90 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { useState, useEffect } from 'react';
+// screens
+import { HomeScreen } from './screens/HomeScreen';
+import { SignUpScreen } from './screens/SignUp';
+import { SignInScreen } from './screens/SignIn';
+// firebase modules
+import { firebaseConfig } from './config/Config';
+import { initializeApp } from 'firebase/app'
+import { 
+  getAuth, 
+  createUserWithEmailAndPassword,
+  onAuthStateChanged,
+  signOut,
+  signInWithEmailAndPassword
+} from "firebase/auth"
 
-//screens
-import { HomeScreen } from './Screens/HomeScreen';
-import { SignUpScreen } from './Screens/SignUp';
+import { 
+  getFirestore,
+  doc,
+  setDoc,
+  addDoc,
+  collection
+} from 'firebase/firestore'
 
 const Stack = createNativeStackNavigator();
 
+const FBapp = initializeApp( firebaseConfig )
+const FBauth = getAuth( FBapp )
+const FBdb = getFirestore( FBapp )
+
 export default function App() {
+  const [auth,setAuth] = useState()
+  const [ errorMsg, setErrorMsg ] = useState()
+
+  onAuthStateChanged( FBauth, (user) => {
+    if( user ) {
+      setAuth( user )
+      console.log( user.uid )
+    }
+    else {
+      setAuth( null )
+    }
+  })
+
+  const SignUp = ( email, password ) => {
+    createUserWithEmailAndPassword( FBauth, email, password )
+    .then( (userCredential) => console.log(userCredential) )
+    .catch( (error) => console.log(error) )
+  }
+
+  const SignIn = ( email, password ) => {
+    signInWithEmailAndPassword( FBauth, email, password )
+    .then( (userCredential) => console.log(userCredential) )
+    .catch( (error) => console.log(error) )
+  }
+
+  const SignOut = () => {
+    signOut(FBauth)
+    .then( () => {
+      //now the user is signed out
+    })
+    .catch((err) => console.log(error) )
+  }
+
+  const AddData = async () => {
+    const userId = auth.uid
+    const path = `users/${userId}/notes`
+    const data = { id: new Date().getTime(), description: "sample data"}
+    const ref = await addDoc( collection( FBdb, path), data )
+  }
+
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="SignUp" component={SignUpScreen} />
-      </Stack.Navigator>        
+     <Stack.Navigator>
+        <Stack.Screen name="Signup">
+          { (props) => <SignUpScreen {...props} handler={SignUp} authStatus={auth} /> }
+        </Stack.Screen>
+        <Stack.Screen name="Signin">
+          { (props) => <SignInScreen {...props} handler={SignIn} authStatus={auth} /> }
+        </Stack.Screen>
+        <Stack.Screen name="Home">
+          { (props) => <HomeScreen {...props} authStatus={auth} signOutHandler={SignOut} add={AddData} /> }
+        </Stack.Screen>
+      </Stack.Navigator>
     </NavigationContainer>
   );
 }
--- Screens/HomeScreen.js
+++ Screens/HomeScreen.js
@@ -1,9 +1,38 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { View, Text, TouchableOpacity, StyleSheet } from "react-native"
+import { useNavigation } from "@react-navigation/native"
+import { useState, useEffect } from 'react'
 
-export function HomeScreen( props ) {
-    return (
-      <View>
-        <Text>Home Screens</Text>
-      </View>
-    );
+export function HomeScreen(props) {
+  const navigation = useNavigation()
+
+  useEffect(() => {
+    if (!props.authStatus) {
+        navigation.reset( { index: 0, routes: [ {name: "Signin"} ] } )
+    }
+  }, [props.authStatus])
+
+  return (
+    <View>
+      <Text>Home Screen</Text>
+      <TouchableOpacity style={styles.button} onPress={() => props.signOutHandler()}>
+        <Text style={styles.buttonText} >Sign out</Text>
+      </TouchableOpacity>
+      <Text>Add some data</Text>
+      <TouchableOpacity style={styles.button} onPress={() => props.add()} >
+        <Text style={styles.buttonText} >Click to add data</Text>
+      </TouchableOpacity>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  button: {
+    backgroundColor: "#000000",
+    padding: 5,
+  },
+  buttonText: {
+    color: "#ffffff",
+    fontSize: 12,
+    textAlign: "center",
   }
+})
